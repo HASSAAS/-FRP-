c84c3e42f22b0ee51eba33ac5beb28dc349bec28
HASSAAS <90961911+HASSAAS@users.noreply.github.com>
2026-07-09 10:43:35 +0800

    Update run.sh

--- frp-client/run.sh
+++ frp-client/run.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 set -e
 
-# Load bashio
-if [ -f /usr/lib/bashio/bashio.sh ]; then
-    source /usr/lib/bashio/bashio.sh
-fi
-
 CONFIG_PATH='/share/frpc.toml'
 DEFAULT_CONFIG_PATH='/frpc.toml'
+
+# Read HA add-on options from /data/options.json
+SERVER_ADDR=$(jq -r '.serverAddr // empty' /data/options.json)
+SERVER_PORT=$(jq -r '.serverPort // empty' /data/options.json)
+AUTH_TOKEN=$(jq -r '.authToken // empty' /data/options.json)
+LOCAL_IP=$(jq -r '.localIP // empty' /data/options.json)
+LOCAL_PORT=$(jq -r '.localPort // empty' /data/options.json)
+PROXY_NAME=$(jq -r '.proxyName // empty' /data/options.json)
+CUSTOM_DOMAIN=$(jq -r '.customDomain // empty' /data/options.json)
 
 if [ ! -f "$CONFIG_PATH" ]; then
     cp $DEFAULT_CONFIG_PATH $CONFIG_PATH
 fi
 
 # Replace configuration placeholders with actual values
-sed -i "s/your_server_addr/$(bashio::config 'serverAddr')/g" $CONFIG_PATH
-sed -i "s/7000/$(bashio::config 'serverPort')/g" $CONFIG_PATH
-sed -i "s/123456789/$(bashio::config 'authToken')/g" $CONFIG_PATH
-sed -i "s/your_local_ip/$(bashio::config 'localIP')/g" $CONFIG_PATH
-sed -i "s/8123/$(bashio::config 'localPort')/g" $CONFIG_PATH
-sed -i "s/your_proxy_name/$(bashio::config 'proxyName')/g" $CONFIG_PATH
-sed -i "s/your_domain/$(bashio::config 'customDomain')/g" $CONFIG_PATH
+sed -i "s/your_server_addr/${SERVER_ADDR}/g" $CONFIG_PATH
+sed -i "s/7000/${SERVER_PORT}/g" $CONFIG_PATH
+sed -i "s/123456789/${AUTH_TOKEN}/g" $CONFIG_PATH
+sed -i "s/your_local_ip/${LOCAL_IP}/g" $CONFIG_PATH
+sed -i "s/8123/${LOCAL_PORT}/g" $CONFIG_PATH
+sed -i "s/your_proxy_name/${PROXY_NAME}/g" $CONFIG_PATH
+sed -i "s/your_domain/${CUSTOM_DOMAIN}/g" $CONFIG_PATH
 
 cd /usr/src
 
